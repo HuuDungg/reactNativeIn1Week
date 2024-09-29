8356a76a154f414431d7f5069d10b565e061dde9
huudungg <huudung2004kt@gmail.com>
2024-09-29 13:34:59 +0700

    navigate screen and send params

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 // In App.js in a new project
-
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen() {
+  const navigation: any = useNavigation();
+  const handleClick = () => {
+    navigation.navigate("Details", { id: 11 });
+  }
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button onPress={handleClick} title='Go to details' />
     </View>
   );
 }
 
 function DetailScreens() {
+  const route: any = useRoute();
+  console.log("check route print id: ", route?.params?.id);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>DetailScreens</Text>
@@ -29,7 +36,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreens} />
+        <Stack.Screen name="Details" component={DetailScreens} />
       </Stack.Navigator>
     </NavigationContainer>
   );
